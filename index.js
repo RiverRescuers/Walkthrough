db5f75ac219f00d46c6c22a50c2040f87048f529
ritvik13sharma <ritvik13sharma@gmail.com>
2023-09-27 22:25:04 +0530

    Chatbot link added

--- index.js
+++ index.js
@@ -84,3 +84,6 @@ var walkthrough = () => {
 }
 
 document.getElementById("b").addEventListener("click",walkthrough);
+document.getElementById("b2").addEventListener("click",() => {
+    window.location.href="https://chacha-chaudhary-web.vercel.app/chatbot";
+})
